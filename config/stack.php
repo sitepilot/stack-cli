a51109cf63dff556b5da8a1766bc258418ac6615
Nick Jansen <n.jansen@sitepilot.io>
2021-09-21 12:05:40 +0200

    Add redis service

--- config/stack.php
+++ config/stack.php
@@ -5,6 +5,7 @@ return [
         new \App\Services\MysqlService,
         new \App\Services\MailhogService,
         new \App\Services\PhpMyAdminService,
+        new \App\Services\RedisService,
         new \App\Services\LshttpdService,
         new \App\Services\CaddyService,
     ]
--- tests/Feature/DisableCommandTest.php
+++ tests/Feature/DisableCommandTest.php
@@ -8,6 +8,14 @@ it('can disable the MySQL service', function () {
     $this->assertConfig('mysql.enabled', false);
 });
 
+it('can disable the Redis service', function () {
+    /** @var Tests\TestCase $this */
+    $this->artisan('disable redis')
+        ->assertExitCode(0);
+
+    $this->assertConfig('redis.enabled', false);
+});
+
 it('can disable the PhpMyAdmin service', function () {
     /** @var Tests\TestCase $this */
     $this->artisan('disable phpmyadmin')
